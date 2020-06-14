695415740607100264b8a4d02f898aed7bf9014b
cyf <1106572144@qq.com>
2020-06-14 15:39:23 +0800

    second

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function renderEditor() {
+    let inputEl = document.querySelector("#default-todo-panel .todo-editor > input");
+
+    //inputEl.onchange = (e) => {
+    //    console.log("text, ", e.target.value);
+    //    // console.log("input change: " , e);
+    // };
+
+    let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
+    addEl.onclick = (e) => {
+        console.log("add click");
+   };  
+}
+
+renderEditor();
